51928438fef2266865d9f74e97599b5e2f80b33e
Andres Scarafia <andres.g.scarafia@gmail.com>
2022-02-23 14:22:04 -0300

    primer commit basico, de estructura?

---
+++ clases.py
@@ -0,0 +1,14 @@
+#clases.py
+
+''' En este archivo incluyo las clases con las que trabajar para este proyecto'''
+
+def class Insumo(Object):
+	def __init__(self, p)
+	    self.precio = p
+
+def class Producto
+
+
+def class Vela(Producto)
+
+def class Difusor(Producto)
