2504fcb32105a3397b75f46b65753e263ddfc83a
Andres Scarafia <andres.g.scarafia@gmail.com>
2022-02-23 15:15:35 -0300

    Commit de desarrollo de clases, Parte 1

--- clases.py
+++ clases.py
@@ -2,13 +2,31 @@
 
 ''' En este archivo incluyo las clases con las que trabajar para este proyecto'''
 
-def class Insumo(Object):
-	def __init__(self, p)
-	    self.precio = p
+class Insumo:
+	def __init__(self, nombre, stock, unidad):
+	    self.nombre = nombre
+	    self.stock = stock
+	    self.unidad = unidad
 
-def class Producto
+	def __str__(self):
+		return f'{self.nombre} - {self.stock} {self.unidad}'
+
+	def __repr__(self):
+		return f'Insumo({self.nombre}, {self.stock}, {self.unidad})'
+
+	def usar(self, cantidad):
+		self.stock -= cantidad
+		print(f'Quedan {self.stock} {self.unidad} de {self.nombre}')
+
+lista_de_insumos_vela = ['cera', 'pabilo', 'chapita', 'vaso', 'esencia']
+lista_de_insumos_difusor = ['alcohol', 'vial', 'tapa', 'esencia difusor']
+
+class Producto:
+	pass
 
 
-def class Vela(Producto)
+class Vela(Producto):
+	pass
 
-def class Difusor(Producto)
+class Difusor(Producto):
+	pass
