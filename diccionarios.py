753df4ee615de4c0a6f553de728579397b8c6d08
Andres Scarafia <andres.g.scarafia@gmail.com>
2022-06-09 12:25:50 -0300

    New approach, diccionarios con los insumos que demanda cada vela y uno para los precios

---
+++ diccionarios.py
@@ -0,0 +1,25 @@
+#!
+
+v_chico = {'cera': 150,
+           'envase': 'vaso chico',
+           'esencia': 15,
+           'pabilo': 10,
+           'ojal': 2,
+           'mejorador': 0.1
+             }
+
+v_whiskey = {'cera': 225,
+             'envase': 'vaso whiskey',
+             'esencia': 22.5,
+             'pabilo': 10,
+             'ojalillo': 2,
+             'mejorador': 0.1
+             }
+
+v_bombe = {'cera': 100,
+              'envase': 'vaso bombe',
+              'esencia': 10,
+              'pabilo': 10,
+              'ojalillo': 1,
+              'mejorador': 0.1
+             }
